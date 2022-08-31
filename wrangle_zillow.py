bc08c6b70358cbc3146a70b348dbf6686e257c36
QinzhiChen <qinzhic@gmail.com>
2022-08-31 16:57:08 -0500

    files modified

--- wrangle_zillow.py
+++ wrangle_zillow.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[13]:
 
 
 import warnings
 warnings.filterwarnings("ignore")
 
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+
+from env import user, password, host
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -16,25 +20,83 @@ import os
 import csv
 
 
-# In[2]:
+# In[14]:
 
 
 def get_connection(db, user=env.user, host=env.host, password=env.password):
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
 
 
-# In[3]:
+# In[15]:
 
 
-def wrangle_zillow():
-    file='zillow2017_df'
+def acquire_zillow():
+    file='zillow2017_df.csv'
     if os.path.isfile(file):
         return pd.read_csv(file)
     else:
-        zillow2017_df = pd.read_sql(('SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips FROM properties_2017 JOIN propertylandusetype USING (propertylandusetypeid) WHERE propertylandusedesc IN ("Single Family Residential")'), get_connection('zillow'))
+        zillow2017_df = pd.read_sql(('SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt,taxamount,fips FROM properties_2017 JOIN propertylandusetype USING (propertylandusetypeid) WHERE propertylandusedesc IN ("Single Family Residential")'), get_connection('zillow'))
         zillow2017_df.to_csv(file,index=False)
     return zillow2017_df
 
+
+# In[28]:
+
+
+def clean_column():
+    zillow2017_df=acquire_zillow()
+    zillow2017_df.rename(columns={'bedroomcnt':'bedroom','bathroomcnt':'bathroom','calculatedfinishedsquarefeet':'sqtft','taxvaluedollarcnt':'taxvalue'},inplace=True)
+    zillow2017_df['fips']= zillow2017_df['fips'].astype(object)
+    zillow2017_df['yearbuilt']=zillow2017_df['yearbuilt'].astype(object)
+    return zillow2017_df
+
+
+# In[70]:
+
+
+def remove_outlier(k):
+    zillow2017_df=clean_column()
+    col_list=zillow2017_df.select_dtypes(float)
+    for col in col_list:
+        q1, q3 = zillow2017_df[col].quantile([.25, .75]) 
+        iqr = q3 - q1  
+        upper_bound = q3 + k * iqr  
+        lower_bound = q1 - k * iqr  
+        zillow2017_df = zillow2017_df[(zillow2017_df[col] > lower_bound) & (zillow2017_df[col] < upper_bound)]
+        zillow2017_df.dropna()
+    return zillow2017_df
+
+
+# In[91]:
+
+
+def get_hist(k):
+    zillow2017_df=remove_outlier(k)
+    plt.figure(figsize=(16, 3))
+    cols = zillow2017_df.select_dtypes(float).columns
+    for i, col in enumerate(cols):
+        plot_num = i + 1 
+        plt.subplot(1, len(cols), plot_num)
+        plt.title(col)
+        zillow2017_df[col].hist(bins=5)
+        plt.grid(False)
+        plt.ticklabel_format(useOffset=False)
+        plt.tight_layout()
+    plt.show()
+
+
+# In[92]:
+
+
+def wrangle_zillow(k):
+    zillow2017_df=remove_outlier(k)
+    get_hist(k)
+    train_validate, zillow_test = train_test_split(zillow2017_df, test_size=.2, random_state=123)
+    zillow_train, zillow_validate = train_test_split(train_validate, test_size=.3, random_state=123)
+    return zillow_train, zillow_validate, zillow_test
+
+
+# In[93]:
 
 # In[ ]:
 
